f02c61d483675dd774b49bc83cd18a52dc25a9c1
edd-ie <ogendoeddie@gmail.com>
2023-03-08 22:35:21 +0300

    Final fronted retouch

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ let div1 = document.getElementById('showcase')
 let css1 = document.getElementById('view1')
 let head = document.getElementById('one')
 let button = document.getElementById('btn1');
+let newSide = document.getElementById('skill-bar');
 
 button.addEventListener("mousedown", function(){
-    alert("Hello There!")
     div1.style.display = "none"
     css1.setAttribute('href', "./new.css");
 })
 
 h1.addEventListener("mouseover", function(e){
-     h1.style.background = "2px, 2px 2px yellow";
+     h1.style.background = "2px, 2px 2px gray";
 })               
 
+newSide.addEventListener("mouseover", function(e){
+    newSide.style.boxShadow = "5px, 5px 5px white";
+}
+)
